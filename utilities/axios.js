3d67b042540eb56e5c873b16b2f70c8c28075d5e
Ekram70 <ekramullah70@gmail.com>
2024-10-06 19:21:07 +0600

    env removed

--- utilities/axios.js
+++ utilities/axios.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.NEXT_PUBLIC_TEST_API_URL;
+const NEXT_PUBLIC_API_URL = 'https://verse-voice-backend.vercel.app/api/v1';
+const NEXT_PUBLIC_TEST_API_URL = 'http://localhost:8000/api/v1';
+
+const BASE_URL = NEXT_PUBLIC_TEST_API_URL;
 
 export default axios.create({
   baseURL: BASE_URL,
