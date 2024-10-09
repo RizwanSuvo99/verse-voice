1b1d805aa03a6b7853082738500e792d9c9e2cf4
RizwanSuvo99 <rizwanshuvo@gmail.com>
2024-10-09 15:45:24 +0600

    Overlay design added for carouselItem

--- components/Catergories/CarouselItem.jsx
+++ components/Catergories/CarouselItem.jsx
@@ -1,4 +1,4 @@
-import { BackgroundImage, Card, Text } from '@mantine/core';
+import { BackgroundImage, Card, Overlay, Text } from '@mantine/core';
 import Link from 'next/link';
 
 const CarouselItem = ({ backUrl, categoryName, categorySize }) => {
@@ -10,10 +10,21 @@ const CarouselItem = ({ backUrl, categoryName, categorySize }) => {
       <Card shadow="sm" padding="lg" radius="md" withBorder className="!p-0">
         <BackgroundImage
           src={backUrl}
-          className="!flex !h-[300px] flex-col !items-center !justify-center"
+          className="!relative !flex !h-[300px] flex-col !items-center !justify-center"
         >
-          <Text className="!mb-2 !text-3xl !font-bold">{categoryName}</Text>
-          <Text className="!text-2xl">{categorySize}</Text>
+          {/* Overlay with opacity */}
+          <Overlay opacity={0.6} color="#000" zIndex={1} />
+
+          {/* Text content */}
+          <Text
+            className="!mb-2 !text-3xl !font-bold !text-white"
+            style={{ zIndex: 2 }}
+          >
+            {categoryName}
+          </Text>
+          <Text className="!text-2xl !text-white" style={{ zIndex: 2 }}>
+            {categorySize}
+          </Text>
         </BackgroundImage>
       </Card>
     </Link>
