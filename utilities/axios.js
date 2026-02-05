65f45ab8a1c06750b6582132c8e4bab85a0a803b
Rizwansuvo99 <rizwanshuvo@gmail.com>
2026-02-05 22:07:04 +0600

    Update production API URL

--- utilities/axios.js
+++ utilities/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const NEXT_PUBLIC_API_URL = 'https://verse-voice-backend.vercel.app/api/v1';
+const NEXT_PUBLIC_API_URL = 'https://backend-five-beta-ey61zod81u.vercel.app/api/v1';
 const NEXT_PUBLIC_TEST_API_URL = 'http://localhost:8000/api/v1';
 
 const BASE_URL = process.env.NODE_ENV === 'production' ? NEXT_PUBLIC_API_URL : NEXT_PUBLIC_TEST_API_URL;
